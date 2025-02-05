1a6cc59cdeca9f16377d17b7b6ef4d419a9b19dd
troybedingfield <troybedingfield@gmail.com>
2025-02-05 13:11:38 -0800

    mobile menu component added

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -5,6 +5,7 @@ import SplitButton from '../SplitButton/SplitButton';
 import './Header.scss';
 import { usePathname, useRouter } from 'next/navigation';
 import path from 'path';
+import MobileMenu from '../MobileMenu/MobileMenu';
 
 export default function Header({ ...props }) {
     const router = useRouter()
@@ -22,15 +23,16 @@ export default function Header({ ...props }) {
             window.open('troy-bedingfield-resume.pdf');
         }
     }
+
     function handleMobileMenuClick(item: any) {
-        if (item === 'Design') {
+        if (item === 'Home') {
+            router.push('/')
+        } else if (item === 'Design') {
             router.push('/design')
         } else if (item === 'LinkedIn') {
             window.open('https://www.linkedin.com/in/troy-bedingfield-0b5850a/', '_blank');
         } else if (item === 'GitHub') {
             window.open('https://github.com/troybedingfield/', '_blank');
-        } else {
-            router.push('/design');
         }
     }
 
@@ -46,16 +48,26 @@ export default function Header({ ...props }) {
     return (
         <header>
             <div className="header-container">
-                <div className="left-header left flex items-center gap-4">
+                <div className="left-header left relative flex items-center gap-4">
+                    <div className='mobile-left-menu'>
+                        <MobileMenu menuItems={['Home', 'Design', 'LinkedIn', 'GitHub']} handleMobileMenuClick={handleMobileMenuClick} />
+                    </div>
                     <div className="header-name text-center" onClick={() => router.push('/')}>Troy Bedingfield</div>
                     <div className='desktop-left-menu'>
-                        {(() => {
+                        {/* {(() => {
                             if (pathname === '/design') {
                                 return <Button fill='clear' customColor='#000000' customBGColor='var(--color-clear-button-hover)' maxHeight={36} classNames='flex items-center'>Design</Button>;
                             } else {
                                 return <Button fill='clear' customColor='#000000' maxHeight={36} classNames='flex items-center' buttonClick={() => router.push('/design')}>Design</Button>;
                             }
-                        })()}
+                        })()} */}
+                        {pathname === '/design' ?
+                            <Button fill='clear' customColor='#000000' customBGColor='var(--color-clear-button-hover)' maxHeight={36} classNames='flex items-center'>Design</Button>
+                            :
+                            <Button fill='clear' customColor='#000000' maxHeight={36} classNames='flex items-center' buttonClick={() => router.push('/design')}>Design</Button>
+
+                        }
+
                     </div>
 
                     {/* <Button fill='clear' customColor='#000000' buttonClick={handleGithubClick}>GitHub</Button> */}
@@ -66,9 +78,9 @@ export default function Header({ ...props }) {
                         <Button fill='clear' customColor='#000000' buttonClick={handleGithubClick}>GitHub</Button>
                     </div>
 
-                    <div className='mobile-right-menu'>
+                    {/* <div className='mobile-right-menu'>
                         <SplitButton dropdownItems={['Design', 'LinkedIn', 'GitHub']} buttonClick={handleMobileMenuClick}></SplitButton>
-                    </div>
+                    </div> */}
                     {/* <a href="https://www.linkedin.com/in/troy-bedingfield-0b5850a/" target="_blank">LinkedIn</a>
                     <a href="https://github.com/troybedingfield/" target="_blank">GitHub</a> */}
 
---
+++ src/app/components/MobileMenu/MobileMenu.tsx
@@ -0,0 +1,110 @@
+'use client';
+import { useEffect, useRef, useState } from 'react';
+import './MobileMenu.scss'
+import { AnimatePresence, motion } from 'motion/react';
+
+
+
+
+
+
+export default function MobileMenu({ ...props }) {
+    const { children, menuItems, handleMobileMenuClick } = props
+
+    const [isOpen, setIsOpen] = useState(false);
+
+
+
+    const [dropdownValue, setDropdownValue] = useState(menuItems[0]);
+
+    // console.log(dropdownValue);
+
+    const dropdown = useRef<any | undefined>();
+
+    // useOutsideClick(dropdown);
+
+    function handleMenuIconClick() {
+        setIsOpen(isOpen => !isOpen);
+        console.log(isOpen)
+    }
+
+    function handleDropdownClick(item: any) {
+        // console.log(item)
+        setDropdownValue(item);
+        handleMobileMenuClick(item);
+        handleMenuIconClick();
+    }
+
+    /**
+ * Hook that alerts clicks outside of the passed ref
+ */
+    // function useOutsideClick(ref: any) {
+
+    //     useEffect(() => {
+    //         function handleClickOutside(event: any) {
+    //             if (ref.current && !ref.current.contains(event.target)) {
+    //                 handleMenuIconClick();
+    //             }
+    //         }
+
+    //         if (ref) {
+    //             // Bind the event listener
+    //             document.addEventListener("mousedown", handleClickOutside);
+    //             return () => {
+    //                 // Unbind the event listener on clean up
+    //                 document.removeEventListener("mouseup", handleClickOutside);
+    //             };
+    //         }
+    //     }, [ref])
+
+    // }
+
+    return (
+
+
+        <div className="mobileMenuContainer flex relative">
+            {/* <button className=""
+                    onClick={() => buttonClick(dropdownValue)}>
+                    {children ? children : dropdownValue}
+                </button> */}
+            <div id="desktop-clear" className='relative flex' onClick={handleMenuIconClick}>
+                <div
+                    id="closeIconDesktop"
+                    className={isOpen ? 'open' : undefined}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+
+            <div className="dropdown">
+
+                {/* <button onClick={handleMenuIconClick}>
+                        <span className="down-arrow"></span>
+                    </button> */}
+                <AnimatePresence>
+                    {!isOpen ? null : (<motion.div key="modal"
+                        initial={{ opacity: 0, height: 0 }}
+                        animate={{ opacity: 1, height: "auto" }}
+                        exit={{ opacity: 0, height: 0 }} className="dropdown-content" ref={dropdown}>
+                        {/* <a >
+                    
+                    </a> */}
+
+                        {menuItems.map((item: any, index: number) => {
+                            return (<motion.a initial={{ opacity: 0 }}
+                                animate={{ opacity: 1, }}
+                                transition={{ delay: 0.1 }}
+                                exit={{ opacity: 0 }} key={index} onClick={() => handleDropdownClick(item)}>{item}</motion.a>)
+                        })}
+
+
+
+                    </motion.div>)}
+                </AnimatePresence>
+            </div>
+        </div>
+
+    )
+}
