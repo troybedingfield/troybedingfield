33ce399102a509028b05771d7ad6249fda59a47d
troybedingfield <troybedingfield@gmail.com>
2025-02-10 10:27:55 -0800

    added animation to carousel slides

--- src/app/components/Carousel/Carousel.tsx
+++ src/app/components/Carousel/Carousel.tsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from 'motion/react';
 import './Carousel.scss'
 import { useCarousel } from './useCarousel';
 
@@ -21,9 +22,12 @@ export default function CarouselNew({ ...props }) {
             onMouseLeave={handleMouseLeave}
             style={{ height: selHeight + 'px' }}>
             <div className="carousel" >
-
-                <img src={`${slide}`} className="slide fade" />
-
+                <AnimatePresence>
+                    <motion.img key={`${slide}`} src={`${slide}`} initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        transition={{ delay: 0.3, duration: 0.3 }} className="slide fade" />
+                </AnimatePresence>
                 <a className="prev" style={{ color: arrowColor }} onClick={back}>&#10094;</a>
                 <a className="next" style={{ color: arrowColor }} onClick={next}>&#10095;</a>
             </div>
